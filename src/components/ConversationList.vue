f288a4f632b8ccbb0b899965badde56eabfbc523
lalize <printf@kakao.com>
2020-07-17 18:02:30 +0900

    refactor: remove vuetify of converstation list

--- src/components/ConversationList.vue
+++ src/components/ConversationList.vue
@@ -1,24 +1,24 @@
 <template>
-    <v-list two-line style="background: inherit">
-        <v-list-item-group style="background: inherit">
-            <div v-for="(conversation, index) in conversations" :key="generate(index)">
-                <ConversationItem :conversation="conversation" />
-                <div class="mb-5"></div>
-            </div>
-        </v-list-item-group>
-    </v-list>
+  <div class="conversations">
+    <ConversationItem
+      v-for="(conversation, index) in conversations"
+      :key="generate(index)"
+      :conversation="conversation"
+    />
+  </div>
 </template>
 
 <script>
-    import { nanoid } from "nanoid"
-    import ConversationItem from "./ConversationItem";
-    export default {
-        props: ['conversations'],
-        components: {
-            ConversationItem
-        },
-        methods: {
-            generate: () => nanoid()
-        }
-    };
+import { nanoid } from "nanoid";
+import ConversationItem from "./ConversationItem";
+
+export default {
+  props: ["conversations"],
+  components: {
+    ConversationItem,
+  },
+  methods: {
+    generate: () => nanoid(),
+  },
+};
 </script>
