bf11108fcc2c1a869fd5c2bdc9ab6aa1ae142f10
lalize <printf@kakao.com>
2020-07-17 17:58:23 +0900

    refactor: remove vuetify of converstation item

--- src/components/ConversationItem.vue
+++ src/components/ConversationItem.vue
@@ -1,17 +1,15 @@
 <template>
-  <v-list-item style="background: #333" class="rounded-xl">
-    <v-list-item-content>
-      <v-list-item-title v-text="conversation.message"></v-list-item-title>
-      <v-list-item-subtitle class="text--primary"
-                            v-text="conversation.userId"></v-list-item-subtitle>
-      <v-list-item-subtitle
-        v-text="$moment(conversation.conversationTime).format('YYYY-MM-DD HH:mm')"></v-list-item-subtitle>
-    </v-list-item-content>
-  </v-list-item>
+  <div class="conversation">
+    <div class="content">
+      <div class="message"></div>
+      <div class="user"></div>
+      <div class="datetime"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    props: ['conversation']
-  };
+export default {
+  props: ["conversation"],
+};
 </script>
