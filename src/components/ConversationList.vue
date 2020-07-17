b4f1c7ba043f300939af18b5fdd90c02ed7a1e40
lalize <printf@kakao.com>
2020-07-17 18:15:59 +0900

    refactor: remove vuetify of Home

--- src/components/ConversationList.vue
+++ src/components/ConversationList.vue
@@ -2,23 +2,19 @@
   <div class="conversations">
     <ConversationItem
       v-for="(conversation, index) in conversations"
-      :key="generate(index)"
+      :key="index"
       :conversation="conversation"
     />
   </div>
 </template>
 
 <script>
-import { nanoid } from "nanoid";
 import ConversationItem from "./ConversationItem";
 
 export default {
   props: ["conversations"],
   components: {
     ConversationItem,
-  },
-  methods: {
-    generate: () => nanoid(),
   },
 };
 </script>
