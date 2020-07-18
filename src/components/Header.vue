982cc2b9ebd4fa2f05e6ff732e95ec9bbce282fe
lalize <printf@kakao.com>
2020-07-19 02:32:59 +0900

    fix: update css to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,20 @@
 <template>
   <header class="header">
-    <div class="menu" @click="$emit('clickMenu')">
-      <img src="../assets/menu-outline.svg" width="32" height="32"/>
-    </div>
-    <div class="service">
-      <RouterLink to="/">
-        <div class="logo">
-          <img src="../assets/logo.svg" width="32" height="32"/>
-        </div>
-      </RouterLink>
-      <RouterLink to="/" class="name">Archive</RouterLink>
+    <div class="nav">
+      <div class="menu" @click="$emit('clickMenu')">
+        <img src="../assets/menu-outline.svg" width="32" height="32" />
+      </div>
+      <div class="service">
+        <RouterLink to="/">
+          <div class="logo">
+            <img src="../assets/logo.svg" width="32" height="32" />
+          </div>
+        </RouterLink>
+        <RouterLink to="/" class="name">Archive</RouterLink>
+      </div>
     </div>
     <div class="rest">
-      <input class="search" placeholder="Search..."/>
+      <input class="search" placeholder="Search..." />
       <button class="submit"></button>
     </div>
   </header>
@@ -21,73 +23,80 @@
 <script></script>
 
 <style scoped>
-  .header {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 80px;
-    background: #000;
-    color: #fff;
-    box-shadow: 0 1px 5px #000;
-    z-index: 99999;
-    padding: 0 20px;
-  }
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 80px;
+  background: #000;
+  color: #fff;
+  box-shadow: 0 1px 5px #000;
+  z-index: 99999;
+  padding: 0 20px;
+}
 
-  .menu {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-  }
+.nav {
+  display: flex;
+}
 
-  .service {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+}
 
-  .service .logo {
-    display: flex;
-    align-items: center;
-    width: 40px;
-    height: 40px;
-  }
+.service {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .service .name {
-    color: #fff;
-    font-size: 24px;
-    font-weight: 600;
-  }
+.service .logo {
+  display: flex;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+}
 
-  .rest {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    margin-left: 10px;
-    flex: 1;
-  }
+.service .name {
+  color: #fff;
+  font-size: 24px;
+  font-weight: 600;
+}
 
-  .search {
-    width: 45vw;
-    height: 40px;
-    border: 0;
-    border-bottom: 1px solid #fff;
-    outline: none;
-    color: #fff;
-    background: #000;
-  }
+.rest {
+  display: flex;
+  justify-self: flex-end;
+  justify-content: flex-end;
+  align-items: center;
+  margin-left: 10px;
+  flex: 0.6;
+}
 
-  .submit {
-    width: 40px;
-    height: 40px;
-    background: url("../assets/search-outline.svg") no-repeat;
-    background-size: 80% 80%;
-    background-position: 5px 5px;
-    border: 0;
-    border-bottom: 1px solid #fff;
-    outline: none;
-    cursor: pointer;
-  }
+.search {
+  flex: 1;
+  height: 40px;
+  border: 0;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  color: #fff;
+  background: #000;
+  border-radius: 0;
+}
+
+.submit {
+  width: 40px;
+  height: 40px;
+  background: url("../assets/search-outline.svg") no-repeat;
+  background-size: 80% 80%;
+  background-position: 5px 5px;
+  border: 0;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  cursor: pointer;
+}
 </style>
