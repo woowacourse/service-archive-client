74167dbc5d7f356565aee4c8a45b133b86fcbe52
lowoon <sangbo0229@gmail.com>
2020-07-21 12:40:36 +0900

    feat: 코드 하이라이트를 적용한다.
    - highlight.js 적용
    - slack codeblock에 언어를 명시할 수 없어 java를 기본 언어로 지정

--- src/components/MarkDown.vue
+++ src/components/MarkDown.vue
@@ -1,68 +1,74 @@
 <template>
-  <div v-html="render" class="message"></div>
+    <div v-html="render" class="message"></div>
 </template>
 
 <script>
-  import marked from "marked"
+    import marked from "marked"
+    import highlight from "highlight.js"
+    import java from "highlight.js/lib/languages/java"
 
-  marked.use({
-    renderer: {
-      code(text) {
-        return `<code><pre>${text.replace(/\n\n/g, "\n")}</pre></code>`
-      }
+    highlight.registerLanguage("java", java);
+
+    marked.use({
+        renderer: {
+            code(text) {
+                return `<code><pre>${highlight.highlight("java", text.replace(/\n\n/g, "\n")).value}</pre></code>`
+            }
+        }
+    })
+    export default {
+        name: "MarkDown",
+        props: {
+            content: {
+                default: "",
+                type: String
+            }
+        },
+        computed: {
+            render() {
+                const result = this.content.replace(/\n/g, '\n\n')
+                    .replace(/```/g, "\n```\n")
+                    .replace(/&gt;/g, ">")
+                    .replace(/&lt;/g, "<")
+                return marked(result)
+            }
+        }
     }
-  })
-  export default {
-    name: "MarkDown",
-    props: {
-      content: {
-        default: "",
-        type: String
-      }
-    },
-    computed: {
-      render() {
-        const result = this.content.replace(/\n/g, '\n\n')
-        .replace(/```/g, "\n```\n")
-        .replace(/&gt;/g, ">")
-        .replace(/&lt;/g, "<")
-        return marked(result)
-      }
-    }
-  }
 </script>
 
 <style>
-  pre {
-    background: #111;
-    overflow: auto;
-    padding: 20px;
-  }
+    @import "~highlight.js/styles/atom-one-dark.css";
 
-  pre > code {
-    font-family: 'Menlo', sans-serif;
-  }
-
-  .message > p {
-    font-family: 'Menlo', sans-serif;
-    color: #ddd;
-    font-size: 20px;
-    padding: 3px;
-  }
-
-  .message a {
-    color: aqua;
-  }
-
-  blockquote {
-    border-left: 3px solid #aaa;
-    background: rgba(255, 255, 255, 0.1);
-    padding: 0 10px;
-  }
-
-  @media screen and (max-width: 768px) {
     pre {
-      padding: 10px;
+        background: #111;
+        overflow: auto;
+        padding: 20px;
     }
-  }
+
+    pre > code {
+        font-family: 'Menlo', sans-serif;
+    }
+
+    .message > p {
+        font-family: 'Menlo', sans-serif;
+        color: #ddd;
+        font-size: 20px;
+        padding: 3px;
+    }
+
+    .message a {
+        color: aqua;
+    }
+
+    blockquote {
+        border-left: 3px solid #aaa;
+        background: rgba(255, 255, 255, 0.1);
+        padding: 0 10px;
+    }
+
+    @media screen and (max-width: 768px) {
+        pre {
+            padding: 10px;
+        }
+    }
 </style>
