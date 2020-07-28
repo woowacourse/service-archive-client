deca76829d54d840af2905d1169221959151d479
이동규 <cu@woowahan.com>
2020-07-28 18:13:11 +0900

    feat: accept external url

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     devServer: {
+        disableHostCheck: true,
+        port: 4000,
         proxy: {
             "/": {
-                target: "http://172.30.1.25:8080", // 개발서버
+                target: "http://172.17.0.1:8081", // 개발서버
             },
         },
     },
